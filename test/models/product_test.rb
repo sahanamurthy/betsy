7256c38cc6002fdc9fc99601c55426a70103446b
Sahana Murthy <s.murthy730.com>
2017-04-19 14:47:16 -0700

    validation tests written and passing

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -9,7 +9,39 @@ describe Product do
         product.valid?.must_equal false
       end
 
+      it "is invalid without a unique name" do
+        product = products(:aliens_two)
+        product.valid?.must_equal false
+      end
 
+      it "is invalid without a quantity" do
+        product = products(:no_quantity)
+        product.valid?.must_equal false
+      end
 
+      it "is invalid with a non-number quantity" do
+        product = products(:odd_quantity)
+        product.valid?.must_equal false
+      end
+
+      it "is invalid without a price" do
+        product = products(:no_price)
+        product.valid?.must_equal false
+      end
+
+      it "is invalid with a non-numerical price" do
+        product = products(:bad_price)
+        product.valid?.must_equal false
+      end
+
+      it "is invalid with a negative price" do
+        product = products(:mean_price)
+        product.valid?.must_equal false
+      end
+
+      it "is invalid without a category" do
+        product = products(:no_category)
+        product.valid?.must_equal false
+      end
   end
 end
