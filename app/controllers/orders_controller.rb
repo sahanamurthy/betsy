389d9232ac69e8b816b69d5ba59e9411956f1c22
Allison Northrop <agnorthrop@gmail.com>
2017-04-20 16:34:23 -0700

    wrote show method for orders controller + tests and routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,8 @@ class OrdersController < ApplicationController
     @orders = Order.all
   end
 
+  def show
+    @order = Order.find(params[:id])
+  end
+
 end
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -3,10 +3,21 @@ require "test_helper"
 describe OrdersController do
 
   describe "index" do
-
     it "gets index" do
       get orders_path
       must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "should get show" do
+      order = Order.first
+      get orders_path(order)
+      must_respond_with :success
+    end
+
+    it "shows an order that exists" do
+      # order = Order.first
     end
 
   end
