adcabbca0c83cf6d4f0d0950df024f6c34ce7f64
Allison Northrop <agnorthrop@gmail.com>
2017-04-21 13:30:41 -0700

    fixed spacing

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-get '/orders/new', to: "orders#new", as: 'new_order'
-get '/orders', to: "orders#index", as: 'orders'
-get '/orders/:id', to: "orders#show", as: 'order'
+  get '/orders/new', to: "orders#new", as: 'new_order'
+  get '/orders', to: "orders#index", as: 'orders'
+  get '/orders/:id', to: "orders#show", as: 'order'
 
 
 
