4bcf5e4793c3b5201d9677d2c40e766825c17dfd
Marisol Lopez <lmarisolopez@gmail.com>
2017-04-22 12:36:17 -0700

    completed validations and association tests

--- test/models/order_product_test.rb
+++ test/models/order_product_test.rb
@@ -1,9 +1,43 @@
 require "test_helper"
 
 describe OrderProduct do
-  let(:order_product) { OrderProduct.new }
 
-  it "must be valid" do
-    value(order_product).must_be :valid?
+  describe "associations" do
+    it "belongs_to product" do
+      ar = OrderProduct.reflect_on_association(:product)
+      ar.macro.must_equal :belongs_to
+    end
+
+    it "belongs_to order" do
+      ar = OrderProduct.reflect_on_association(:order)
+      ar.macro.must_equal :belongs_to
+    end
   end
+  describe "validations" do
+    it "quantity must be present" do
+      order_product = OrderProduct.create
+      result = order_product.valid?
+      result.must_equal false
+    end
+    it "checks quantity to make sure numericality" do
+      order_product = OrderProduct.create(quantity: "apple")
+      result = order_product.valid?
+      result.must_equal false
+    end
+    it "product_id must be present" do
+      order_product = OrderProduct.create(quantity: 2, order_id: orders(:one).id)
+      result = order_product.valid?
+      result.must_equal false
+      order_product.errors.messages.must_include :product_id
+    end
+
+    it "order_id must be present" do
+      order_product = OrderProduct.create(quantity: 2, product_id: products(:one).id)
+      result = order_product.valid?
+      result.must_equal false
+      # order_product.errors.messages.must_equal({})
+      order_product.errors.messages.must_include :order_id
+    end
+  end
+
 end
