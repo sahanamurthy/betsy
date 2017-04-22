26091853ade7c8ca81dfaa80f0d0a99d7582258a
Tehut Getahun <tehut.m.g@gmail.com>
2017-04-22 08:32:23 -0700

    added edit and delete tests and methods on product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,55 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    if @product.save
+      redirect_to products_path
+    else
+
+      render :new, status: :bad_request
+      flash[:failure] = "Could not add your product"
+      flash[:messages] = @product.errors.messages
+    end
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    head :not_found if @product.nil?
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update_attributes(product_params)
+    @product.save
+
+      if @product.save
+        redirect_to product_path(params[:id])
+      else
+        render :edit, status: :bad_request
+      end
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+  def product_params
+    return params.require(:product).permit(:name, :quantity, :category, :price, :description, :merchant_id)
+  end
 end
