f9b0f4075d31fdeb5cea340d8298425da34ecd33
Tehut Getahun <tehut.m.g@gmail.com>
2017-04-26 10:25:07 -0700

    sessions controller new user integration test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,17 +5,28 @@ describe SessionsController do
 
   describe "auth_callback" do
     it"registers a new user" do
+      start_count = Merchant.count
+      merchant = Merchant.new(username:"test user", email:"test@user.net", oauth_provider: "github", oauth_uid:9999999)
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(merchant))
+      get auth_callback_path(:github)
+      must_redirect_to root_path
+
+      #This string will only show up if the test fails becasuse the merchant_id passed was not recieved
+      session[:merchant_id].must_equal Merchant.last.id, "Could not log in"
+
+      Merchant.count.must_equal start_count + 1
 
     end
 
     it "accepts a returning user" do
       start_count = Merchant.count
       merchant = merchants(:grace)
-     OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(merchant))
-     get auth_callback_path(:github)
-     must_redirect_to root_path
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(merchant))
+      get auth_callback_path(:github)
+      must_redirect_to root_path
 
-     session[:merchant_id].must_equal merchant.id, "Merchant was not logged in"
+      session[:merchant_id].must_equal merchant.id, "Merchant was not logged in"
 
       Merchant.count.must_equal start_count
 
